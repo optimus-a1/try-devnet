5663e05d114c2a63adc2a0b1c4328d7563a8dbd5
optimus-a1 <154238957+optimus-a1@users.noreply.github.com>
2025-04-23 17:41:57 +0800

    Update wallet.sh

--- packages/common/wallet.sh
+++ packages/common/wallet.sh
@@ -25,7 +25,7 @@ check_balance() {
 dev_wallet() {
     print_step "1" "Generating new dev wallet"
     # CAUTION: DO NOT GENERATE A KEYPAIR LIKE THIS FOR PRODUCTION
-    local keypair=$(scast wallet new)
+    local keypair=$(cast wallet new)
     DEV_WALLET_ADDRESS=$(echo "$keypair" | grep "Address:" | awk '{print $2}')
     DEV_WALLET_PRIVKEY=$(echo "$keypair" | grep "Private key:" | awk '{print $3}')
     if [ -z "$DEV_WALLET_ADDRESS" ]; then
